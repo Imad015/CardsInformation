7cf17a602a78817020ca1b49f32068a8769ecdfa
Moulay Imad <111585460+Imad015@users.noreply.github.com>
2026-04-14 16:34:54 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+struct informationInfo{
+    string firstName;
+    string lastName;
+    int age;
+    string phone;
+};
+
+void readInformation(informationInfo &person){
+    cout<<"enter your firstname: \n";
+    cin>>person.firstName;
+    cout<<"enter your lastname: \n";
+    cin>>person.lastName;
+    cout<<"enter your age: \n";
+    cin>>person.age;
+    cout<<"enter your phone: \n";
+    cin>>person.phone;
+}
+
+void writeInformation(informationInfo person){
+    cout<<"*****************************"<<endl;
+    cout<<"Firstname: "<<person.firstName<<endl;
+    cout<<"Lastname: "<<person.lastName<<endl;
+    cout<<"age: "<<person.age<<endl;
+    cout<<"phone: "<<person.phone<<endl;
+    cout<<"*****************************"<<endl;
+}
+
+void readInformationtoArray(informationInfo arr[],int numberOfCards){
+    for(int i =0; i<numberOfCards;i++){
+        cout<<"Person "<<i<<": \n";
+        readInformation(arr[i]);
+
+    }
+
+}
+
+void writeInformationtoArray(informationInfo arr[],int numberOfCards){
+    for(int i=0;i<numberOfCards;i++){
+            cout<<"Person "<<i<<": \n";
+            writeInformation(arr[i]);
+    }
+
+}
+
+int main(){
+    int numberOfCards = 0;
+    cout<<"enter number Cards: \n";
+    cin>>numberOfCards;
+    informationInfo arr[numberOfCards];
+    readInformationtoArray(arr,numberOfCards);
+    writeInformationtoArray(arr,numberOfCards);
+    return 0;
+
+}
